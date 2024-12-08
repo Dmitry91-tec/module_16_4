874971614158e6c6bf3a64b5f16447d462dbe05f
Dmitry <nasdi91@yandex.ru>
2024-12-08 16:04:58 +1000

    Add files via upload

--- module_16_4.py
+++ module_16_4.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,status,Body,HTTPException
-from pydantic import BaseModel                             #базовая модель дляудобногопредставления данных
+from pydantic import BaseModel                             #базовая модель для удобного представления данных
 from typing import List
 
 app = FastAPI()
@@ -17,7 +17,10 @@ async def get_all_messages() -> List[User]:             #список объек
 
 @app.post("/user/{username}/{age}")
 async def create_message(message: User) -> str:
-    message.id = len(users)
+    if len(users) == 0:
+        message.id = len(users) + 1
+    else:
+        message.id = users[-1].id+1
     users.append(message)
     return f"User {message} is registered"
 
@@ -28,14 +31,17 @@ async def update_message(user_id: int, username: str, age: int) -> str:
             if user_.id == user_id:
                 user_.username = username
                 user_.age = age
-                return f"User {user_.id} has been updated"
+                return f"User {user_} has been updated"
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
 
 @app.delete("/user/{user_id}")
 async def delete_message(user_id: int) -> str:
     try:
-        users.pop(user_id)
-        return f"User {user_id} has been deleted"                          #если такое сообщение не нашлось,
+        for user_ in users:
+            if user_.id == user_id:
+                message = users[user_id-1]
+                del users[user_id-1]
+                return f"User {message} has been deleted"                          #если такое сообщение не нашлось,
     except IndexError:                                                     #то срабатывает исключение
         raise HTTPException(status_code=404, detail="User was not found")
